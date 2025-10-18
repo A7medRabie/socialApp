a3565d53306db4cc31459f702735684b542ea470
A7medRabie <a.rabie1001@gmail.com>
2025-10-18 19:05:46 +0300

    navbar design

--- src/app/core/layouts/auth-layout/auth-layout.component.ts
+++ src/app/core/layouts/auth-layout/auth-layout.component.ts
@@ -4,7 +4,7 @@ import { RouterOutlet } from "@angular/router";
 
 @Component({
   selector: 'app-auth-layout',
-  imports: [NavbarComponent, RouterOutlet],
+  imports: [ RouterOutlet],
   templateUrl: './auth-layout.component.html',
   styleUrl: './auth-layout.component.css'
 })
